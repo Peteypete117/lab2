5a1897f327dc13395796db61b13355cc9e503d03
Peteypete117 <71951435+Peteypete117@users.noreply.github.com>
2021-03-04 17:45:59 -0500

    hello lab 2
    sorry for the late

--- balancer.js
+++ balancer.js
@@ -1,7 +1,41 @@
 var http = require('http');
+var i = 0
+var serverArray = [3333,2222,1111]
 
-http.createServer(function (req, res) {
-  //Call server 1 or 2 or 3
-  res.end();
-}).listen(9999);
+http.createServer( callServer ).listen(9999);
 console.info("Server running ....");
+
+function callServer(req, res)
+{
+  if(req.url == "/hello")
+  {
+    if(i == 2)
+    {
+      i = 0 
+    }
+    else
+    {
+      i += 1
+    }
+    http.get("http://localhost:" + serverArray[i], function(response) 
+    {
+      var str = '';
+      //another chunk of data has been received, so append it to `str`
+      response.on('data', function (chunk) 
+      {
+        str += chunk;
+      });
+      
+      //the whole response has been received, so we just print it out here
+      response.on('end', function () 
+      { 
+        res.writeHead(200,{"Content-Type":"text/html"})
+        res.end(str)
+      });
+    }).on('error', function (e) 
+    {
+      callServer(req, res)
+    });
+  }
+}
+  
